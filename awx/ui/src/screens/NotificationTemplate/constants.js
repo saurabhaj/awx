ceb8b44ec70fa8e8134eb78855126bca446183b3
Saurabh Jadhav <saurabh.jadhav@aptech.ac.in>
2024-11-13 10:47:48 +0530

    first

---
+++ awx/ui/src/screens/NotificationTemplate/constants.js
@@ -0,0 +1,13 @@
+/* eslint-disable-next-line import/prefer-default-export */
+export const NOTIFICATION_TYPES = {
+  awssns: 'AWS SNS',
+  email: 'Email',
+  grafana: 'Grafana',
+  irc: 'IRC',
+  mattermost: 'Mattermost',
+  pagerduty: 'Pagerduty',
+  rocketchat: 'Rocket.Chat',
+  slack: 'Slack',
+  twilio: 'Twilio',
+  webhook: 'Webhook',
+};
